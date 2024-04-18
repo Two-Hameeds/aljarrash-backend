823b2aefe4633e8b4ed60b32ea31de33d9544dc0
Hameed M <s201424820@gmail.com>
2024-04-18 16:15:21 +0300

    add whitenoise

--- aljarrash_backend/settings.py
+++ aljarrash_backend/settings.py
@@ -159,7 +159,7 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-# STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 STATIC_URL = 'static/'
 # STATIC_ROOT = 'static'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
