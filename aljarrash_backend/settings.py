47a3281db6358b5d6cd945176f7feb1015af251d
Hameed M <s201424820@gmail.com>
2024-04-19 00:42:02 +0300

    use postgreSQL instead of sqlite3

--- aljarrash_backend/settings.py
+++ aljarrash_backend/settings.py
@@ -92,14 +92,18 @@ WSGI_APPLICATION = 'aljarrash_backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
-print(os.getenv("DBSTRING", "env var not added"))
+
+DATABASES = {
+    'default': dj_database_url.parse(os.environ.get("DBSTRING"))
+}
+print(os.getenv(DATABASES, "env var not added"))
 
 # DATABASES = {
 #     'default': {
