a04f3b8336b920f3d776c00dd0442278add25296
Hameed M <s201424820@gmail.com>
2024-05-08 11:43:02 +0300

    add local secret key

--- aljarrash_backend/settings.py
+++ aljarrash_backend/settings.py
@@ -11,8 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1#+oydk16!*stgh2h@89k-cjkk0u)0e#)-%+*j$6i3qe=vdtc7'
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-1#+oydk16!*stgh2h@89k-cjkk0u)0e#)-%+*j$6i3qe=vdtc7')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
