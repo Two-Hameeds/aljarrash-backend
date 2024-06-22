f8c773ffb37528d307d5cfdf40b3a91288ad4959
s201424820 <s201424820@gmail.com>
2024-06-22 03:06:53 +0000

    try to deploy the project using uvicorn instead of gunicorn

--- startup.sh
+++ startup.sh
@@ -1,5 +1,6 @@
 # startup.sh is used by infra/resources.bicep to automate database migrations and isn't used by the sample application
 python manage.py migrate
-gunicorn --workers 2 --threads 4 --timeout 60 --access-logfile \
-    '-' --error-logfile '-' --bind=0.0.0.0:8000 \
-     --chdir=/home/site/wwwroot aljarrash_backend.wsgi
+# gunicorn --workers 2 --threads 4 --timeout 60 --access-logfile \
+#     '-' --error-logfile '-' --bind=0.0.0.0:8000 \
+#      --chdir=/home/site/wwwroot aljarrash_backend.wsgi
+uvicorn your_project_name.asgi:application --host 0.0.0.0 --port 8000 --workers 2
