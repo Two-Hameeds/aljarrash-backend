a2ac62d7359bbb278d292f0d472053310a7bd89a
s201424820 <s201424820@gmail.com>
2024-07-06 05:57:28 +0000

    try to fix static files

--- aljarrash_backend/settings.py
+++ aljarrash_backend/settings.py
@@ -195,7 +195,7 @@ AWS_S3_ENDPOINT_URL = 'https://sgp1.digitaloceanspaces.com'
 AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
 # static settings
 AWS_LOCATION = 'static'
-STATIC_URL = f'https://{AWS_S3_ENDPOINT_URL}/{AWS_LOCATION}/'
+STATIC_URL = f'https://{AWS_STORAGE_BUCKET_NAME}.{AWS_S3_ENDPOINT_URL}/{AWS_LOCATION}/'
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 STATICFILES_DIRS = (BASE_DIR / 'static',)
